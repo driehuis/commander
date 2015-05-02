c62913bcee93b63ea89cd6d2d9d661069e837839
Bert Driehuis <bert@cyso.nl>
2015-05-02 20:45:16 +0200

    Sync with reality: Rework exception handling Give feedback via $0

--- lib/commander.rb
+++ lib/commander.rb
@@ -9,6 +9,7 @@ class Commander < ActiveResource::Base
     self.user = config['user']
     self.password = config['pass']
     self.element_name = "command"
+    self.format = :xml
     
     begin
       self.find(:all, :conditions => { :exitstatus => nil}).each do | item |
@@ -16,6 +17,8 @@ class Commander < ActiveResource::Base
           running.push item.id
           pid = Process.fork {
             Process.fork {
+              desc = item.command.length > 60 ? item.command[0,56] + "..." : item.command
+              $0 = "commander (worker #{item.id}) #{desc}"
               output=`#{item.command}`
               item.exitstatus = $?.exitstatus
               item.output = output
@@ -24,13 +27,10 @@ class Commander < ActiveResource::Base
               until saved
                 begin
                   saved = item.save
-                rescue Exception => e
+                rescue StandardError => e
                     DaemonKit.logger.info("unknown error on save item #{item.id}: #{e}")
-                    if e.message == "exit"
-                      exit
-                    end
                 end
-                if retrytime < 7200
+                if retrytime < 14400
                   retrytime = retrytime * 2
                 else
                   DaemonKit.logger.info("Giving up on item #{item.id}") unless saved
@@ -40,7 +40,7 @@ class Commander < ActiveResource::Base
                 sleep retrytime unless saved
               end
             }
-            exit
+            exit 0
           }
           DaemonKit.logger.info("Started process #{pid} for item #{item.id}: #{item.command}")
           Process.detach pid
@@ -48,6 +48,10 @@ class Commander < ActiveResource::Base
       end
     rescue Errno::ECONNREFUSED
       DaemonKit.logger.info("Connection refused")
+      sleep config['interval']
+      retry
+    rescue ActiveResource::ServerError => e
+      DaemonKit.logger.info("Server error: #{e.message}")
       sleep config['interval']
       retry
     rescue ActiveResource::ResourceNotFound
@@ -58,11 +62,7 @@ class Commander < ActiveResource::Base
       DaemonKit.logger.info("Timed out, will try again")
       sleep config['interval']
       retry
-    rescue Exception => e
-      if e.message == "exit"
-        DaemonKit.logger.info("exit message received")
-        exit
-      end
+    rescue StandardError => e
       DaemonKit.logger.info("Unknown error: #{e.message}")
       sleep config['interval']
       retry
